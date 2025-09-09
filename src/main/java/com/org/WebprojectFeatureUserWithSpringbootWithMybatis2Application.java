1eda26c7f399431a0711c41db745d02e960db427
JHPC003\hnjh1 <hnjh12195@naver.com>
2025-09-09 17:31:09 +0900

    main

---
+++ src/main/java/com/org/WebprojectFeatureUserWithSpringbootWithMybatis2Application.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprojectFeatureUserWithSpringbootWithMybatis2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebprojectFeatureUserWithSpringbootWithMybatis2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/webprojectfeatureuserwithspringbootwithmybatis2/WebprojectFeatureUserWithSpringbootWithMybatis2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.webprojectfeatureuserwithspringbootwithmybatis2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprojectFeatureUserWithSpringbootWithMybatis2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
